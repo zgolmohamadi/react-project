5f2780a04ecbe030298df7019060bf66247e0aef
unknown <zgh402@gmail.com>
2023-12-14 20:23:22 +0330

    show chart by click button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import * as React from "react";
+import Button from "@mui/joy/Button";
+import Modal from "@mui/joy/Modal";
+import ModalClose from "@mui/joy/ModalClose";
+import Chart from "./componnet/chart";
+import { data } from "./data/contstants";
+import { DialogContent, DialogTitle, ModalDialog } from "@mui/joy";
+import "./assets/styles/index.css";
 
-import './App.css'
-
-function App() {
-
+export default function App() {
+  const [open, setOpen] = React.useState<boolean>(false);
   return (
-    <>
-     <button>chart</button>
-    </>
-  )
+    <React.Fragment>
+      <Button variant="outlined" color="neutral" onClick={() => setOpen(true)}>
+        Open modal
+      </Button>
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <ModalDialog layout="fullscreen">
+          <ModalClose variant="outlined" />
+          <DialogTitle>Chart</DialogTitle>
+          <DialogContent>
+            <div className="chart-container">
+              <Chart data={data} />
+            </div>
+          </DialogContent>
+        </ModalDialog>
+      </Modal>
+    </React.Fragment>
+  );
 }
-
-export default App
---
+++ src/data/contstants.ts
@@ -0,0 +1,272 @@
+export const data=[
+    {
+      "id": "japan",
+      "color": "hsl(260, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 154
+        },
+        {
+          "x": "helicopter",
+          "y": 230
+        },
+        {
+          "x": "boat",
+          "y": 172
+        },
+        {
+          "x": "train",
+          "y": 197
+        },
+        {
+          "x": "subway",
+          "y": 92
+        },
+        {
+          "x": "bus",
+          "y": 92
+        },
+        {
+          "x": "car",
+          "y": 154
+        },
+        {
+          "x": "moto",
+          "y": 273
+        },
+        {
+          "x": "bicycle",
+          "y": 277
+        },
+        {
+          "x": "horse",
+          "y": 40
+        },
+        {
+          "x": "skateboard",
+          "y": 191
+        },
+        {
+          "x": "others",
+          "y": 74
+        }
+      ]
+    },
+    {
+      "id": "france",
+      "color": "hsl(293, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 123
+        },
+        {
+          "x": "helicopter",
+          "y": 52
+        },
+        {
+          "x": "boat",
+          "y": 210
+        },
+        {
+          "x": "train",
+          "y": 237
+        },
+        {
+          "x": "subway",
+          "y": 38
+        },
+        {
+          "x": "bus",
+          "y": 99
+        },
+        {
+          "x": "car",
+          "y": 101
+        },
+        {
+          "x": "moto",
+          "y": 54
+        },
+        {
+          "x": "bicycle",
+          "y": 238
+        },
+        {
+          "x": "horse",
+          "y": 230
+        },
+        {
+          "x": "skateboard",
+          "y": 17
+        },
+        {
+          "x": "others",
+          "y": 28
+        }
+      ]
+    },
+    {
+      "id": "us",
+      "color": "hsl(268, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 224
+        },
+        {
+          "x": "helicopter",
+          "y": 93
+        },
+        {
+          "x": "boat",
+          "y": 173
+        },
+        {
+          "x": "train",
+          "y": 203
+        },
+        {
+          "x": "subway",
+          "y": 169
+        },
+        {
+          "x": "bus",
+          "y": 198
+        },
+        {
+          "x": "car",
+          "y": 181
+        },
+        {
+          "x": "moto",
+          "y": 133
+        },
+        {
+          "x": "bicycle",
+          "y": 125
+        },
+        {
+          "x": "horse",
+          "y": 142
+        },
+        {
+          "x": "skateboard",
+          "y": 240
+        },
+        {
+          "x": "others",
+          "y": 209
+        }
+      ]
+    },
+    {
+      "id": "germany",
+      "color": "hsl(166, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 181
+        },
+        {
+          "x": "helicopter",
+          "y": 248
+        },
+        {
+          "x": "boat",
+          "y": 290
+        },
+        {
+          "x": "train",
+          "y": 199
+        },
+        {
+          "x": "subway",
+          "y": 288
+        },
+        {
+          "x": "bus",
+          "y": 96
+        },
+        {
+          "x": "car",
+          "y": 229
+        },
+        {
+          "x": "moto",
+          "y": 182
+        },
+        {
+          "x": "bicycle",
+          "y": 298
+        },
+        {
+          "x": "horse",
+          "y": 72
+        },
+        {
+          "x": "skateboard",
+          "y": 249
+        },
+        {
+          "x": "others",
+          "y": 159
+        }
+      ]
+    },
+    {
+      "id": "norway",
+      "color": "hsl(208, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 251
+        },
+        {
+          "x": "helicopter",
+          "y": 22
+        },
+        {
+          "x": "boat",
+          "y": 134
+        },
+        {
+          "x": "train",
+          "y": 240
+        },
+        {
+          "x": "subway",
+          "y": 16
+        },
+        {
+          "x": "bus",
+          "y": 264
+        },
+        {
+          "x": "car",
+          "y": 73
+        },
+        {
+          "x": "moto",
+          "y": 225
+        },
+        {
+          "x": "bicycle",
+          "y": 72
+        },
+        {
+          "x": "horse",
+          "y": 30
+        },
+        {
+          "x": "skateboard",
+          "y": 8
+        },
+        {
+          "x": "others",
+          "y": 252
+        }
+      ]
+    }
+  ]
